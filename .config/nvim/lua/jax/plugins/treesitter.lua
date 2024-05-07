5cd6be4cf8abc3746bf168d4cdbe84f9a5026114
Jax Young <jaxvanyang@gmail.com>
2024-05-07 15:41:36 +0800

    backup(nvim): enable indentation & folding of nvim-treesitter

--- .config/nvim/lua/jax/plugins/treesitter.lua
+++ .config/nvim/lua/jax/plugins/treesitter.lua
@@ -6,6 +6,15 @@ return {
 		highlight = {
 			enable = true,
 		},
+		indent = {
+			enable = true,
+		},
 	},
 	build = ":TSUpdate",
+	init = function()
+		-- configure folding
+		vim.opt.foldmethod = "expr"
+		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+		vim.opt.foldenable = false
+	end,
 }
