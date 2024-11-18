354ecd0d4c652da7bfc873cf87576a0aca9698ab
Jax Young <jaxvanyang@gmail.com>
2024-11-19 07:16:16 +0800

    fish: split configs

---
+++ .config/fish/conf.d/10_abbr.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+	# Git
+	if command -v git &> /dev/null
+		abbr ga git add
+		abbr gcmsg git commit -m
+		abbr gfa git fetch --all --prune --jobs=5
+		abbr gl git pull
+		abbr gp git push
+		abbr grst git restore --staged
+		abbr gst git status
+		abbr gsc git switch -c
+	end
+
+	# Python
+	abbr pdb python3 -m pdb
+	abbr py python3
+
+	# Neovim
+	abbr v nvim
+
+	# Misc
+	abbr l ll
+end
