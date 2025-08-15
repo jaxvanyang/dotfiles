0c46dcafb4e8d76e306a7516912c8e10d22861bc
Jax Young <jaxvanyang@gmail.com>
2025-08-15 17:17:02 +0800

    updconf(gradle): add more Maven mirrors

--- .gradle/init.gradle.kts
+++ .gradle/init.gradle.kts
@@ -1,7 +1,17 @@
 allprojects {
 	repositories {
 		maven {
+			url = uri("https://maven.proxy.ustclug.org/maven2")
+		}
+		maven {
+			url = uri("https://mirrors.163.com/maven/repository/maven-public")
+		}
+		maven {
 			url = uri("https://maven.aliyun.com/repository/public")
+		}
+		// Fabric-Loom
+		maven {
+			url = uri("https://repository.hanbings.io/proxy")
 		}
 		mavenLocal()
 		mavenCentral()
