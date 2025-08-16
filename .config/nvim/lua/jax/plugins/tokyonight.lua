cf5e7f35403ba2800125cc59a98d08bd16006096
Jax Young <jaxvanyang@gmail.com>
2025-08-16 20:43:14 +0800

    updconf(nvim): make TokyoNight transparent

--- .config/nvim/lua/jax/plugins/tokyonight.lua
+++ .config/nvim/lua/jax/plugins/tokyonight.lua
@@ -2,7 +2,14 @@ return {
 	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
-	config = function()
+	init = function()
 		vim.cmd([[colorscheme tokyonight]])
 	end,
+	opts = {
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+	},
 }
