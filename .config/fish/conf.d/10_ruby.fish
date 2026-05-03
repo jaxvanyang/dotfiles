bf23344397c4b36ff84ed47b27aad02ce343afd2
Jax Young <jaxvanyang@gmail.com>
2026-05-03 13:18:41 +0800

    fish: only use Ruby Gems on Linux

--- .config/fish/conf.d/10_ruby.fish
+++ .config/fish/conf.d/10_ruby.fish
@@ -1,5 +1,6 @@
 if status is-interactive
-	if command -q gem
+	if [ "$(uname)" = Linux ]
+		and command -q gem
 		set -gx GEM_HOME "$(gem env user_gemhome)"
 		fish_add_path -g "$GEM_HOME/bin"
 	end
